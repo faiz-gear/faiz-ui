de11086bc138f624b5d1cb76fb0b1fa45eb409e5
faiz-gear <1824056030@qq.com>
2025-03-09 00:07:49 +0800

    docs(ripple): remove TypeScript ignore comments and update ESLint config

--- apps/docs/components/docs/ripple/custom-color.tsx
+++ apps/docs/components/docs/ripple/custom-color.tsx
@@ -3,9 +3,7 @@ import { useRef } from "react"
 import { Ripple } from "@faiz-ui/react"
 
 export default function CustomColor() {
-	// @ts-ignore
 	const rippleRef = useRef<any>(null)
-	// @ts-ignore
   const handleClick = (e: any) => {
     rippleRef.current?.addRipple?.(e)
   }
